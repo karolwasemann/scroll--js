7fd75788ef91f7136b2f81d82877f2f58f98b60e
karolwasemann <kw.kwaq@gmail.com>
2020-11-16 12:33:16 +0100

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,38 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const div = document.createElement("div");
+const span = document.createElement("span");
+
+document.body.appendChild(div);
+document.body.appendChild(span);
+
+let height = 0;
+div.style.background = "red";
+div.style.height = `${height}px`;
+
+let grow = true;
+
+window.addEventListener("scroll", function () {
+  if (height == 0) {
+    grow = true;
+  } else if (height >= window.innerHeight / 2) {
+    grow = false;
+  }
+
+  if (grow == true) {
+    height += 5;
+    div.style.height = `${height}px`;
+    div.style.background = "red";
+    span.style.top = `${height}px`;
+    span.style.background = "green";
+  } else if (grow == false) {
+    height -= 5;
+    div.style.height = `${height}px`;
+    div.style.background = "green";
+    span.style.top = `${height}px`;
+    span.style.background = "red";
+  }
+});
